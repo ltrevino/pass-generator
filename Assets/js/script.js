328a70204fa23fbeecf75949497acdf65304eb63
luis <luis@MacBook-Pro-de-luis.local>
2021-07-17 20:09:58 -0500

    Comments on js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,10 +11,11 @@ function writePassword() {
 
 }
 
+//Generate secure password
 function generatePassword(){
 
+  //Global Variables
   var length;
-  
   var upper;
   var lower;
   var numeric;
@@ -22,11 +23,14 @@ function generatePassword(){
   var alphabet='';
   var pass='';
 
+  //Routines to define length and Alphabet
   defineLength();
   defineChars();
 
+  //Define password length
   function defineLength(){
 
+    //Iterates until the pass is between 8 and 128
     length = prompt("Introduce your password length between 8 and 128:");
     if (8 <= length && length<=128)
     {
@@ -39,13 +43,16 @@ function generatePassword(){
 
   }
 
+  //Function to determine the Alphabet
   function defineChars(){
   
+    //Prompts to let the user select the characters to be used
     lower = confirm("Would you like to include lowercase?");
     upper = confirm("Would you like to include upercase?");
     numeric = confirm("Would you like to include numeric?");
     special = confirm("Would you like to include special chars?");
 
+    //Validates that at least 1 set of characters is selected
     if ( lower || upper || numeric || special ){
         return;
     }
@@ -57,6 +64,7 @@ function generatePassword(){
   
   } 
 
+  //Builds the alphabet based in the user selection
   if(numeric){
     alphabet="1234567890";
   }
@@ -75,12 +83,15 @@ function generatePassword(){
 
   alphabet=alphabet.split('');
 
+  //Randomize the characters and build the pass based on the user length selection.
   for (var x=0; x<length ; x++)
   {
     var random = Math.floor(Math.random() * alphabet.length);
     pass+=alphabet[random];
 
   }
+
+  //Shows the results in an alert and in the html
   alert("This is your password: "+pass);
   return pass;
 
